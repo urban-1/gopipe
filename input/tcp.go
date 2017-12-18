81c7f73174de7a12ea16f80819713c6d744d967f
andreas.bontozoglou <andreas.bontozoglou@sky.uk>
2017-12-18 12:34:31 +0000

    TCP/UDP _private fields (host/port)

--- input/tcp.go
+++ input/tcp.go
@@ -127,7 +127,7 @@ func (p *TCPJSONInput) handleRequest(conn net.Conn) {
         }
 
         e := NewEvent(json_data)
-        json_data["_from"] = conn.RemoteAddr().String()
+        json_data["_from_addr"], json_data["_from_port"], _ = net.SplitHostPort(conn.RemoteAddr().String())
         p.OutQ<-e
 
         tmpdata = []byte{}
